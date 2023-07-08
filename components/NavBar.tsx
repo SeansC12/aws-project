f1e546574e5f458f53e5a06942ac59f3869561b2
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2023-07-08 17:58:05 +0800

    worked on header

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,90 +11,116 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
+
 import Link from "next/link";
+import Image from "next/image";
 import Logo from "../public/vercel.svg";
 
 export default function NavBar() {
   return (
-    <NavigationMenu className="border-b-[1px] border-fiord-300 h-16 justify-between pr-20 w-full bg-white bg-opacity-80 backdrop-blur-md">
-      <NavigationMenuList className="pl-20">
-        <NavigationMenuItem>
-          <a href="/">
-            <img src={Logo.src} className="h-10" alt="ES" />
-          </a>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-      <NavigationMenuList className="">
-        <NavigationMenuItem>
+    <div className="border-b-[1px] border-gray-300 w-[100vw]">
+      <NavigationMenu className="h-16 w-full absolute bg-white bg-opacity-80 backdrop-blur-md">
+        <div>
+          <NavigationMenuList className="pl-20">
+            <NavigationMenuItem>
+              <a href="/">
+                <Image
+                  src={Logo.src}
+                  height={50}
+                  width={50}
+                  alt="ES"
+                />
+              </a>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </div>
+        <div>
+          <NavigationMenuList>
+            {/* <NavigationMenuItem>
           <Link href="/" legacyBehavior passHref>
             <NavigationMenuLink
               className={`${navigationMenuTriggerStyle()} text-esorange 2xl:text-[1.2rem] bg-transparent`}
             >
-              Home
+              Find
             </NavigationMenuLink>
           </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="2xl:text-[1.2rem] bg-transparent">
-            Properties
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="flex justify-end">
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/listings"
+        </NavigationMenuItem> */}
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="2xl:text-[1.2rem] bg-transparent">
+                Find
+              </NavigationMenuTrigger>
+              <NavigationMenuContent className="flex justify-end">
+                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  <li className="row-span-3">
+                    <NavigationMenuLink asChild>
+                      <Link
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        href="/listings"
+                      >
+                        <div className="mb-2 mt-4 text-lg font-medium">
+                          Properties
+                        </div>
+                      </Link>
+                    </NavigationMenuLink>
+                  </li>
+                  <ListItem href="/listings" title="Rent">
+                    Find a room to rent
+                  </ListItem>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="2xl:text-[1.2rem] left bg-transparent">
+                Company
+              </NavigationMenuTrigger>
+              <NavigationMenuContent className="flex justify-end">
+                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  <li className="row-span-3">
+                    <NavigationMenuLink asChild>
+                      <Link
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        href="/listings"
+                      >
+                        <div className="mb-2 mt-4 text-lg font-medium">
+                          Our company
+                        </div>
+                      </Link>
+                    </NavigationMenuLink>
+                  </li>
+                  <ListItem
+                    href="/about-us"
+                    title="About Us"
                   >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      Properties
-                    </div>
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/listings" title="Rent">
-                Find a room to rent
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="2xl:text-[1.2rem] bg-transparent">
-            Company
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className="flex justify-end">
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/listings"
+                    Our mission, our values
+                  </ListItem>
+                  <ListItem href="/why-us" title="Why Us">
+                    Why choose us?
+                  </ListItem>
+                  <ListItem
+                    href="/contact-us"
+                    title="Contact Us"
                   >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      Our company
-                    </div>
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/about-us" title="About Us">
-                Our mission, our values
-              </ListItem>
-              <ListItem href="/why-us" title="Why Us">
-                Why choose us?
-              </ListItem>
-              <ListItem
-                href="/contact-us"
-                title="Contact Us"
-              >
-                Got questions or feedback? Drop us a line,
-                anytime.
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem></NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+                    Got questions or feedback? Drop us a
+                    line, anytime.
+                  </ListItem>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </div>
+        <div>
+          <Avatar className="relative right-5">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            {/* <AvatarFallback>CN</AvatarFallback> */}
+          </Avatar>
+        </div>
+      </NavigationMenu>
+    </div>
   );
 }
 
