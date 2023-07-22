97bb829df2cf7a4f45b79fff2de33b40647a49d1
simonfalke-01 <ziqibrandonli@gmail.com>
2023-07-22 18:35:37 +0800

    Add example

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,5 @@ import { JobPostForm } from "@/components/JobPostForm";
 
 export default function Home() {
   return <div className="">
-    <div className="flex justify-center items-center h-[100vh]"><JobPostForm /></div>
   </div>
 }
